853eaaffacb6df122416716fddf5b11d36ef5c05
drak3hft7 <86606985+drak3hft7@users.noreply.github.com>
2022-04-20 16:25:23 +0200

    Update Subscan4.sh

--- Subscan4.sh
+++ Subscan4.sh
@@ -102,7 +102,7 @@ do
     if [ ! -f "live-subfinder-httpx-$DOMAIN.txt" ] || [ "$overwrite" = true ]
     then
         echo -e "$orange[Httpx] in progress with the results Subfinder...";
-        httpx -silent -no-color -l "$outputDir$inputdirectory/subfinder-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,8000,8080,443,8443,4443,4433 -threads 25 -o "$outputDir$inputdirectory/subfinder-httpx-$DOMAIN.txt" > /dev/null 2>&1;
+        httpx -silent -no-color -l "$outputDir$inputdirectory/subfinder-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,81,82,88,135,143,300,443,554,591,593,832,902,981,993,1010,1024,1311,2077,2079,2082,2083,2086,2087,2095,2096,2222,2480,3000,3128,3306,3333,3389,4243,4443,4567,4711,4712,4993,5000,5001,5060,5104,5108,5357,5432,5800,5985,6379,6543,7000,7170,7396,7474,7547,8000,8001,8008,8014,8042,8069,8080,8081,8083,8085,8088,8089,8090,8091,8118,8123,8172,8181,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9100,9200,9443,9800,9981,9999,10000,10443,12345,12443,16080,18091,18092,20720,28017,49152 -threads 25 -o "$outputDir$inputdirectory/subfinder-httpx-$DOMAIN.txt" > /dev/null 2>&1;
         cut -d' ' -f1 < "$outputDir$inputdirectory/subfinder-httpx-$DOMAIN.txt" | sort -u > "$outputDir$inputdirectory/live-subfinder-httpx-$DOMAIN.txt"
     fi
     	echo -e "$blue[OK Subfinder] scan performed.";
@@ -112,7 +112,7 @@ do
     if [ ! -f "live-assetfinder-httpx-$DOMAIN.txt" ] || [ "$overwrite" = true ]
     then
         echo -e "$orange[Httpx] in progress with the results Assetfinder...";
-        httpx -silent -no-color -l "$outputDir$inputdirectory/assetfinder-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,8000,8080,443,8443,4443,4433 -threads 25 -o "$outputDir$inputdirectory/assetfinder-httpx-$DOMAIN.txt" > /dev/null 2>&1;
+        httpx -silent -no-color -l "$outputDir$inputdirectory/assetfinder-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,81,82,88,135,143,300,443,554,591,593,832,902,981,993,1010,1024,1311,2077,2079,2082,2083,2086,2087,2095,2096,2222,2480,3000,3128,3306,3333,3389,4243,4443,4567,4711,4712,4993,5000,5001,5060,5104,5108,5357,5432,5800,5985,6379,6543,7000,7170,7396,7474,7547,8000,8001,8008,8014,8042,8069,8080,8081,8083,8085,8088,8089,8090,8091,8118,8123,8172,8181,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9100,9200,9443,9800,9981,9999,10000,10443,12345,12443,16080,18091,18092,20720,28017,49152 -threads 25 -o "$outputDir$inputdirectory/assetfinder-httpx-$DOMAIN.txt" > /dev/null 2>&1;
         cut -d' ' -f1 < "$outputDir$inputdirectory/assetfinder-httpx-$DOMAIN.txt" | sort -u > "$outputDir$inputdirectory/live-assetfinder-httpx-$DOMAIN.txt"
     fi
     	echo -e "$blue[OK Assetfinder] scan performed.";
@@ -122,7 +122,7 @@ do
     if [ ! -f "live-amass-httpx-$DOMAIN.txt" ] || [ "$overwrite" = true ]
     then
         echo -e "$orange[Httpx] in progress with the results Amass...";
-        httpx -silent -no-color -l "$outputDir$inputdirectory/amass-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,8000,8080,443,8443,4443,4433 -threads 25 -o "$outputDir$inputdirectory/amass-httpx-$DOMAIN.txt" > /dev/null 2>&1;
+        httpx -silent -no-color -l "$outputDir$inputdirectory/amass-$DOMAIN.txt" -title -tech-detect -content-length -web-server -status-code -ports 80,81,82,88,135,143,300,443,554,591,593,832,902,981,993,1010,1024,1311,2077,2079,2082,2083,2086,2087,2095,2096,2222,2480,3000,3128,3306,3333,3389,4243,4443,4567,4711,4712,4993,5000,5001,5060,5104,5108,5357,5432,5800,5985,6379,6543,7000,7170,7396,7474,7547,8000,8001,8008,8014,8042,8069,8080,8081,8083,8085,8088,8089,8090,8091,8118,8123,8172,8181,8222,8243,8280,8281,8333,8443,8500,8834,8880,8888,8983,9000,9043,9060,9080,9090,9091,9100,9200,9443,9800,9981,9999,10000,10443,12345,12443,16080,18091,18092,20720,28017,49152 -threads 25 -o "$outputDir$inputdirectory/amass-httpx-$DOMAIN.txt" > /dev/null 2>&1;
         cut -d' ' -f1 < "$outputDir$inputdirectory/amass-httpx-$DOMAIN.txt" | sort -u > "$outputDir$inputdirectory/live-amass-httpx-$DOMAIN.txt"
     fi
     	echo -e "$blue[OK Amass] scan performed."
